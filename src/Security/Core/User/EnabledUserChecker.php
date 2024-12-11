8535c74f3b0c2c8ff469eb2c7343b0f5edc7f906
siganushka <siganushka@gmail.com>
2024-12-11 16:09:04 +0800

    feat: add EnabledUserChecker

---
+++ src/Security/Core/User/EnabledUserChecker.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siganushka\UserBundle\Security\Core\User;
+
+use Siganushka\Contracts\Doctrine\EnableInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class EnabledUserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user): void
+    {
+        if ($user instanceof EnableInterface && !$user->isEnabled()) {
+            throw new CustomUserMessageAccountStatusException('Your account has been disabled.');
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user): void
+    {
+    }
+}
