bf37025275d35ff185a8f337b2ba389c16c4a220
siganushka <siganushka@gmail.com>
2025-07-24 17:42:06 +0800

    refactor: add missing data_class options

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -8,6 +8,7 @@ use Siganushka\Contracts\Doctrine\ResourceInterface;
 use Siganushka\UserBundle\Entity\User;
 use Siganushka\UserBundle\Form\Type\RepeatedPasswordType;
 use Siganushka\UserBundle\Form\Type\UserIdentifierType;
+use Siganushka\UserBundle\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -19,6 +20,10 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
+    public function __construct(private readonly UserRepository $repository)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -50,6 +55,7 @@ class UserType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            'data_class' => $this->repository->getClassName(),
             'constraints' => new UniqueEntity('identifier', entityClass: User::class),
             'validation_groups' => function (FormInterface $form) {
                 $data = $form->getData();
