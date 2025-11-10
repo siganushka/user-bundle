2752885e813d44db80aef44986442e36ce4e2241
siganushka <siganushka@gmail.com>
2025-11-10 10:41:50 +0800

    refactor: improve UserType

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -35,6 +35,7 @@ class UserType extends AbstractType
             ])
             ->add('enabled', CheckboxType::class, [
                 'label' => 'generic.enabled',
+                'required' => false,
             ])
         ;
     }
